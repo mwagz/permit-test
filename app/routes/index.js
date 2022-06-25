24c6c0d03e4ee1735dc293fcb5228355a64845e9
Matt Wagner <matt.a.wagner4@gmail.com>
2022-06-25 16:12:39 -0400

    Deployed b5eb84b

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-import { QUESTIONS } from './-utils/questions';
-
-export default class IndexRoute extends Route {
-  model() {
-    return QUESTIONS;
-  }
-}
